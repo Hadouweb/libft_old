a60b73b16c8673e96235b47fbb674a6b20210e9f
Hadouweb <kengo14@hotmail.fr>
2015-08-20 18:58:54 +0200

    ft_memcpy ft_strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,27 +1,17 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mel-mouk <mel-mouk@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/12/23 18:31:55 by mel-mouk          #+#    #+#             */
-/*   Updated: 2014/12/23 18:35:25 by mel-mouk         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 
-char	*ft_strchr(const char *s, int c)
+char    *ft_strchr(const char *s, int c)
 {
-	int i;
+    size_t  i;
 
-	if ((char)c == '\0')
-		return ((char *)(s + ft_strlen(s)));
-	i = 0;
-	while (s[i] != (char)c && s[i] != '\0')
-		i++;
-	if (s[i] == (char)c)
-		return ((char*)(s + i));
-	return (NULL);
+    i = 0;
+    if ((char)c == '\0')
+        return ((char *)(s + ft_strlen(s)));
+    while (s[i])
+    {
+        if ((char)s[i] == (char)c)
+            return ((char *)(s + i));
+        i++;
+    }
+    return (0);
 }
