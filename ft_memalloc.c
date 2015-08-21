72fb8f68a8af6a8d8dcab1d9982a4acff6656be7
Hadouweb <kengo14@hotmail.fr>
2015-08-22 01:02:49 +0200

    ft_memalloc ft_memdel

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -1,23 +1,11 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_memalloc.c                                      :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mel-mouk <mel-mouk@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/12/23 23:35:55 by mel-mouk          #+#    #+#             */
-/*   Updated: 2014/12/23 23:41:52 by mel-mouk         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 
-void	*ft_memalloc(size_t size)
+void    *ft_memalloc(size_t size)
 {
-	void	*mem;
+    void    *data;
 
-	if (!(mem = (void*)malloc(sizeof(void) * size)))
-		return (NULL);
-	ft_bzero(mem, size);
-	return (mem);
+    if ((data = (void *)malloc(size * sizeof(*data))) == NULL)
+        return (NULL);
+    ft_memset(data, 0, size);
+    return (data);
 }
