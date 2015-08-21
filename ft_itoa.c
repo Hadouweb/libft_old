d3d3e9d37e6dc07382f811513a2c1d813707266d
Hadouweb <kengo14@hotmail.fr>
2015-08-22 00:45:31 +0200

    ft_itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,6 +1,4 @@
 #include "libft.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 int     ft_intlen(int n)
 {
@@ -8,11 +6,8 @@ int     ft_intlen(int n)
 
     i = 0;
     if (n < 0)
-    {
         i++;
-        n *= -1;
-    }
-    while (n > 0)
+    while (n != 0)
     {
         n /= 10;
         i++;
@@ -24,35 +19,21 @@ char    *ft_itoa(int n)
 {
     char    *str;
     size_t  i;
+    int     sign;
 
     i = ft_intlen(n);
-    printf("i %zu\n", i);
+    sign = (n < 0) ? -1 : 1;
     if (n == 0)
         return ("0");
     if ((str = (char *)malloc(i * sizeof(char) + 1)) == NULL)
         return (NULL);
     if (n < 0)
-    {
         str[0] = '-';
-        n *= -1;
-    }
-    while (i > 0)
+    while (n != 0)
     {
-        str[i - 1] = n % 10 + '0';
-        printf("%zu %c\n", i, str[i]);
+        str[i - 1] = (n % 10 * sign) + '0';
         n /= 10;
         i--;
     }
     return (str);
-}
-
-int     main(int argc, char **argv)
-{
-    int     n;
-    if (argc > 1)
-    {
-        n = ft_atoi(argv[1]);
-        printf("rep : %s\n", ft_itoa(n));
-    }
-    return (0);
 }
