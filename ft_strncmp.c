a2a8dfff03cd508bb7f4ab4ed6fd473f7a9d3ee3
Hadouweb <kengo14@hotmail.fr>
2015-08-21 10:54:51 +0200

    ft_strncmp ft_strncpy

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,30 +1,15 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strncmp.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mel-mouk <mel-mouk@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/12/23 18:54:14 by mel-mouk          #+#    #+#             */
-/*   Updated: 2014/12/23 18:55:12 by mel-mouk         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 
-int ft_strncmp(const char *s1, const char *s2, size_t n)
+int     ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t i;
+    size_t  i;
 
-	i = 0;
-	while (s1[i] == s2[i] && s1[i] != '\0' && i < n)
-		i++;
-	if (i == n)
-		return (0);
-	if ((unsigned char)s1[i] - (unsigned char)s2[i] < 0)
-		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-	else if ((unsigned char)s1[i] - (unsigned char)s2[i] > 0)
-		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+    i = 0;
+    while ((unsigned char)s1[i] == (unsigned char)s2[i] && s1[i] && i < n)
+        i++;
+    if (i == n)
+        return ((unsigned char)s1[i - 1] - (unsigned char)s2[i - 1]);
+    else
+        return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+
 }
-
