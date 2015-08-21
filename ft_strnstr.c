9413b8592a78057f14674465e4e090a5665eff36
Hadouweb <kengo14@hotmail.fr>
2015-08-21 15:31:00 +0200

    ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,40 +1,28 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strnstr.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mel-mouk <mel-mouk@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/12/23 18:47:22 by mel-mouk          #+#    #+#             */
-/*   Updated: 2014/12/23 18:49:26 by mel-mouk         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t n)
+char    *ft_strnstr(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
-	int		y;
+    size_t  i;
+    size_t  j;
 
-	i = 0;
-	y = 0;
-	if (needle[0] == '\0')
-		return ((char *)haystack);
-	if (n == 0)
-		return (NULL);
-	while (haystack[i] != '\0' && i < n)
-	{
-		if (haystack[i] == needle[y])
-		{
-			while (haystack[i] == needle[y] && needle[y] != '\0' && i < n)
-				i++, y++;
-			if (needle[y] == '\0')
-				return ((char*)(haystack + i - y));
-			i = i - y + 1;
-			y = 0;
-		}
-		i++;
-	}
-	return (NULL);
+    i = 0;
+    if (!s2[0])
+        return ((char *)s1);
+    while (s1[i] && i < n)
+    {
+        j = 0;
+        if (s2[j] == s1[i])
+        {
+            while (s2[j] == s1[i] && s2[j] && i < n)
+            {
+                i++;
+                j++; 
+            }
+            if (s2[j] == '\0')
+                return ((char *)s1 + i - j);
+            i = i - (j - 1);
+        }
+        i++;
+    }
+    return (NULL);
 }
