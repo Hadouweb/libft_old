81cd569948f3862971b30965cd66833d27f68eb1
Hadouweb <kengo14@hotmail.fr>
2015-08-21 10:47:23 +0200

    ft_strlen ft_strncat

--- ft_strncat.c
+++ ft_strncat.c
@@ -1,19 +1,16 @@
-#include <string.h>
+#include "libft.h"
 
 char    *ft_strncat(char *dest, const char *src, size_t n)
 {
     size_t  i;
     size_t  j;
 
-    j = 0;
     i = 0;
+    j = 0;
     while (dest[i])
         i++;
     while (src[j] && j < n)
-    {
-        dest[i + j] = src[j];
-        j++;
-    }
-    dest[i + j] = '\0';
+        dest[i++] = src[j++];
+    dest[i] = '\0';
     return (dest);
 }
