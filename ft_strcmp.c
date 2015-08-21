3241c7fc2670771bfaa760bf349106ebc4a9a71b
Hadouweb <kengo14@hotmail.fr>
2015-08-21 09:36:59 +0200

    ft_strcmp ft_strcpy

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -1,23 +1,11 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mel-mouk <mel-mouk@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/12/23 18:49:33 by mel-mouk          #+#    #+#             */
-/*   Updated: 2014/12/23 18:54:06 by mel-mouk         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 
-int	ft_strcmp(const char *s1, const char *s2)
+int     ft_strcmp(const char *s1, const char *s2)
 {
-	int i;
+    size_t  i;
 
-	i = 0;
-	while ((unsigned char)s1[i] == (unsigned char)s2[i] && s1[i] != '\0')
-		i++;
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+    i = 0;
+    while ((unsigned char)s1[i] == (unsigned char)s2[i] && s1[i])
+        i++;
+    return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
