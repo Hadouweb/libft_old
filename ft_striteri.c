5909fda89162621aa00b7b4ed26e600d7a158d99
Hadouweb <kengo14@hotmail.fr>
2015-08-24 08:16:04 +0200

    ft_striter ft_striteri

--- ft_striteri.c
+++ ft_striteri.c
@@ -1,28 +1,13 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_striteri.c                                      :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mel-mouk <mel-mouk@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/12/23 23:57:58 by mel-mouk          #+#    #+#             */
-/*   Updated: 2014/12/24 00:00:17 by mel-mouk         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 
-void	ft_striteri(char *s, void (*f)(unsigned int, char *))
+void    ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
-	int i;
+    size_t  i;
 
-	if (s && f)
-	{
-		i = 0;
-		while (s[i] != '\0')
-		{
-			f(i, &s[i]);
-			i++;
-		}
-	}
+    i = 0;
+    while (s[i])
+    {
+        (*f)(i, &s[i]);
+        i++;
+    }
 }
