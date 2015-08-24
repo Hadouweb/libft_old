0dacd890cefd6965a6b543ee747e7ad6c0871f26
Hadouweb <kengo14@hotmail.fr>
2015-08-24 08:25:13 +0200

    ft_strjoin ft_strmap

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,31 +1,27 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strjoin.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mel-mouk <mel-mouk@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/12/24 00:07:29 by mel-mouk          #+#    #+#             */
-/*   Updated: 2014/12/24 00:09:25 by mel-mouk         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char    *ft_strjoin(char const *s1, char const *s2)
 {
-	int		i;
-	char	*str;
+    size_t  i;
+    size_t  j;
+    char    *str;
 
-	if (s1 || s2)
-	{
-		i = ft_strlen(s1) + ft_strlen(s2);
-		str = ft_strnew(i);
-		if (s1)
-			ft_strcpy(str, s1);
-		if (s2)
-		ft_strcpy((str + ft_strlen(s1)), s2);
-		return (str);
-	}
-	return (NULL);
+    i = 0;
+    j = 0;
+    if ((str = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) *
+                    sizeof(char) + 1)) == NULL)
+        return (NULL);
+    while (s1[i])
+    {
+        str[i] = s1[i];
+        i++;
+    }
+    while (s2[j])
+    {
+        str[i] = s2[j];
+        i++;
+        j++;
+    }
+    str[i] = '\0';
+    return (str);
 }
--- ft_strmap.c
+++ ft_strmap.c
@@ -1,34 +1,17 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strmap.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mel-mouk <mel-mouk@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/12/24 00:00:20 by mel-mouk          #+#    #+#             */
-/*   Updated: 2014/12/24 00:01:26 by mel-mouk         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 
-char	*ft_strmap(char const *s, char (*f)(char))
+char    *ft_strmap(char const *s, char (*f)(char))
 {
-	char	*str;
-	int		len;
-	int		i;
+    size_t  i;
+    char    *str;
 
-	if (s && f)
-	{
-		len = ft_strlen(s);
-		str = (char*)malloc(sizeof(char*) * len);
-		i = 0;
-		while (s[i] != '\0')
-		{
-			str[i] = f(s[i]);
-			i++;
-		}
-		return (str);
-	}
-	return ((char*)s);
+    i = 0;
+    if ((str = (char *)malloc(ft_strlen(s) * sizeof(char) + 1)) == NULL)
+        return (NULL);
+    while (s[i])
+    {
+        str[i] = (*f)(s[i]);
+        i++;
+    }
+    return (str);
 }
