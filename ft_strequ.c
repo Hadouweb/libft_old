bccce3e62210f2845cf196f8b5f7c63188120d3f
Hadouweb <kengo14@hotmail.fr>
2015-08-24 08:11:46 +0200

    ft_strclr ft_strdel ft_strequ

--- ft_strequ.c
+++ ft_strequ.c
@@ -1,26 +1,11 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strequ.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mel-mouk <mel-mouk@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/12/24 00:02:38 by mel-mouk          #+#    #+#             */
-/*   Updated: 2014/12/24 00:03:51 by mel-mouk         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 
-int		ft_strequ(char const *s1, char const *s2)
+int     ft_strequ(char const *s1, char const *s2)
 {
-	int i;
+    int i;
 
-	if (s1 && s2)
-	{
-		i = ft_strcmp(s1, s2);
-		if (i == 0)
-			return (1);
-	}
-	return (0);
+    if (s1 && s2)
+        if (ft_strcmp(s1, s2) == 0)
+            return (1);
+    return (0);
 }
